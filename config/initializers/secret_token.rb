b3786853ace4db892f5d05a66fa395d1d94db4fd
phillyslick <noesis@philadelphiaslick.com>
2013-05-29 21:21:45 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Philaspice::Application.config.secret_token = 'ef4588b42c1b6e960e69a5573f6f6ca92abf8996efac80a6cb4e4f1eeafa4e4e29c03a905e372be8afc301068b18009aa58649ada6df9da88f23a803328d332c'
