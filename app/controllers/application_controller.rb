7b80dfdc8a66df67aee9840165cae69d4a2dc7a0
phillyslick <noesis@philadelphiaslick.com>
2013-06-20 12:23:50 -0400

    backend filtering is better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < ActionController::Base
   
   def find_products
     if params[:query]
-      @products = Product.search(params[:query])
+      if params[:product] && params[:product][:category_id].present?  
+        category = Category.find(params[:product][:category_id])
+        params[:active] == 'false' ? building_products = category.products.where("name @@ :q", q: params[:query]).inactive : building_products = category.products.where("name @@ :q", q: params[:query]).active
+        params[:stocked] == 'false' ? @products = building_products.where("name @@ :q", q: params[:query]).unstocked : @products = building_products.where("name @@ :q", q: params[:query]).is_stocked
+      elsif params[:category_id].present?
+        category = Category.find(params[:category_id])
+        params[:active] == 'false' ? building_products = category.products.where("name @@ :q", q: params[:query]).inactive : building_products = category.products.where("name @@ :q", q: params[:query]).active
+        params[:stocked] == 'false' ? @products = building_products.where("name @@ :q", q: params[:query]).unstocked : @products = building_products.where("name @@ :q", q: params[:query]).is_stocked
+      else
+        params[:active] == 'false' ? building_products = Product.where("name @@ :q", q: params[:query]).inactive : building_products = Product.where("name @@ :q", q: params[:query]).active
+        params[:stocked] == 'false' ? @products = building_products.where("name @@ :q", q: params[:query]).unstocked : @products = building_products.where("name @@ :q", q: params[:query]).is_stocked
+      end
     elsif params[:product] && params[:product][:category_id].present?  
       category = Category.find(params[:product][:category_id])
       params[:active] == 'false' ? building_products = category.products.inactive : building_products = category.products.active
