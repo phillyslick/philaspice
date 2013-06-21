bae6eb9f74e0348208ac019b31c2cecda99990b8
phillyslick <noesis@philadelphiaslick.com>
2013-06-21 13:17:37 -0400

    Order stats filled out a lil more

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,10 @@
 class Customer < ActiveRecord::Base
   attr_accessible :email, :first_name, :home_phone, :last_name, :work_phone, :company
   validates_presence_of :email, :first_name, :last_name, :home_phone
+
+
+  def name
+    [first_name, last_name].join(" ")
+  end
+
 end
